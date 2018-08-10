054b2b92d9fb36c7f973b8f21edc024ea749a1c2
ecmain <eric.c.main@state.or.us>
2018-08-10 12:00:47 -0700

    Update

--- GIS_Example.R
+++ GIS_Example.R
@@ -1,8 +1,6 @@
 ## Install the tidycensus package if you haven't yet
 #install.packages("tidycensus")
 
-# This is Kevin's comments on your code! Haha!
-
 library(tidycensus)
 library(ggplot2)
 library(dplyr)
